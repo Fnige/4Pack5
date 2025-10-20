cadef38968945a7452afeb854619604b1ea88a00
Fnige <35159947+Fnige@users.noreply.github.com>
2025-10-20 22:05:12 +1100

    Armor poser + some configs

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -289,4 +289,13 @@ ServerEvents.recipes(event => {
 			}
 		]
 	})
+
+	event.shapeless(
+		Item.of('patchouli:guide_book[patchouli:book="advancedperipherals:manual"]'),
+		[
+			"minecraft:iron_ingot",
+			"minecraft:book",
+			"minecraft:light_blue_dye"
+		]
+	)
 })
