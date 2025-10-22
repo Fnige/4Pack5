60131d201cfeaf9adc1773d21fce621559770196
Fnige <35159947+Fnige@users.noreply.github.com>
2025-10-22 23:13:55 +1100

    Sophisticated X recipes and create integration

--- kubejs/server_scripts/sophisticated.js
+++ kubejs/server_scripts/sophisticated.js
@@ -1,4 +1,5 @@
 ServerEvents.recipes(event => {
+	//#region -- Storage
 	function copperise(arr) {
 		event.remove({ output: `sophisticatedstorage:${arr[0]}iron${arr[1]}` })
 
@@ -41,4 +42,61 @@ ServerEvents.recipes(event => {
 	for (let index = 0; index < copper.length; index++) {
 		copperise(copper[index])
 	}
+	//#endregion
+
+	//#region -- Backpacks
+	let bprem = [
+		"sophisticatedbackpacks:iron_backpack",
+		"sophisticatedbackpacks:tool_swapper_upgrade"
+	]
+
+	for (let index = 0; index < bprem.length; index++) {
+		event.remove({ output: bprem[index] })
+	}
+
+	event.custom({
+		"type": "sophisticatedbackpacks:backpack_upgrade",
+		"conditions": [
+			{
+				"type": "sophisticatedcore:item_enabled",
+				"itemRegistryName": "sophisticatedbackpacks:iron_backpack"
+			}
+		],
+		"pattern": [
+			"CCC",
+			"CSC",
+			"CCC"
+		],
+		"key": {
+				"C": {
+					"item": "minecraft:iron_ingot"
+				},
+				"S": {
+					"item": "sophisticatedbackpacks:copper_backpack"
+				}
+		},
+		"result": {
+				"id": "sophisticatedbackpacks:iron_backpack"
+		}
+	})
+
+	event.shaped(
+		Item.of("sophisticatedbackpacks:tool_swapper_upgrade", 1),
+		[
+			"RWR",
+			"PTA",
+			"ISI"
+		],
+		{
+			// Base
+			R: "minecraft:redstone",
+			I: "minecraft:iron_ingot",
+			T: "sophisticatedbackpacks:upgrade_base",
+			// Tools
+			W: "minecraft:golden_sword",
+			A: "minecraft:golden_axe",
+			S: "minecraft:golden_shovel",
+			P: "minecraft:golden_pickaxe"
+		}
+	)
 })
