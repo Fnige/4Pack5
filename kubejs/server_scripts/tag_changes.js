38e17e76b7d9a8faceb91cfb5b4c3c69fba14874
Fnige <35159947+Fnige@users.noreply.github.com>
2025-11-19 22:01:42 +1100

    tag stuff

---
+++ kubejs/server_scripts/tag_changes.js
@@ -0,0 +1,4 @@
+ServerEvents.tags('item', event => {
+	event.add("supplementaries:lunch_basket_blacklist", "create:sand_paper")
+	event.add("supplementaries:lunch_basket_blacklist", "create:red_sand_paper")
+})
